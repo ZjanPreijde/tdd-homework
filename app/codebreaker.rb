f7af3b3553c73159da5a4ea8edcb77c663eb0faf
ZjanPreijde <j.preijde2@upcmail.nl>
2018-02-04 12:14:59 +0100

    Made some methods and var names more descriptive, added some SRP

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -20,53 +20,57 @@ class Codebreaker
     end
 
     def return_result
-      return "Try guessing a number with four digits" if !correct_input?
+      return "Try guessing a number with four digits" if !correct_format?
+      @result = ""
       check_input
     end
 
-    def correct_input?
-      correct_length? && correct_format?
+    def correct_format?
+      correct_length? && is_all_digits?
     end
 
     def correct_length?
       @input.length == REQUIRED_INPUT_LENGTH
     end
 
-    def correct_format?
+    def is_all_digits?
       !@input[/\d{#{REQUIRED_INPUT_LENGTH}}/].nil?
     end
 
     def check_input
-      @result = ""
       check_for_exacts
       check_for_numbers
       return @result
     end
 
     def check_for_exacts
-      (0..REQUIRED_INPUT_LENGTH - 1).each do | n |
-        check_exact(n)
+      (0..REQUIRED_INPUT_LENGTH - 1).each do | input_pos |
+        check_exact(input_pos)
       end
     end
 
-    def check_exact(n)
-      return if @secret_number[n] != @input[n]
-      @input[n]         = " "
-      @secret_number[n] = " "
-      @result          += "+"
+    def check_exact(input_pos)
+      secret_pos = input_pos
+      return if @secret_number[secret_pos] != @input[input_pos]
+      process_result(input_pos, secret_pos, "+")
     end
 
     def check_for_numbers
-      (0..REQUIRED_INPUT_LENGTH - 1).each do |n|
-        check_number(n) if @input[n] != " "
+      (0..REQUIRED_INPUT_LENGTH - 1).each do |input_pos|
+        check_number(input_pos) if @input[input_pos] != " "
       end
     end
 
-    def check_number(n)
-      found_at = @secret_number.index(@input[n])
-      return if found_at.nil?
-      @secret_number[found_at] = " "
-      @result                 += "-"
+    def check_number(input_pos)
+      secret_pos = @secret_number.index(@input[input_pos])
+      return if secret_pos.nil?
+      process_result(input_pos, secret_pos, "-")
+    end
+
+    def process_result(input_pos, secret_pos, result)
+      @input[input_pos]          = " "
+      @secret_number[secret_pos] = " "
+      @result                   += result
     end
 
   end
